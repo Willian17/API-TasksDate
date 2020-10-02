4602dc5263f2a0a3bb924ea950d628ad8cc1b2a0
Willian17 <willianfernandes113@gmail.com>
2020-10-02 11:58:42 -0400

    deploy

---
+++ ormconfig.js
@@ -0,0 +1,11 @@
+console.log('process.env.DATABASE_URL >> ' + process.env.DATABASE_URL)
+module.exports = {
+  "type": "postgres",
+  "url": process.env.DATABASE_URL,
+  "entities": [process.env.DIRECTORY_MODELS],
+  "migrations": [process.env.DIRECTORY_MIGRATIONS],
+  "cli": {
+    "migrationsDir": "./src/database/migrations"
+  }
+}
+
