0cbe1afbb0d1780f476d82251bd4f58cf19f259f
Willian17 <willianfernandes113@gmail.com>
2020-10-02 12:53:00 -0400

    desisto

--- dist/src/server.js
+++ dist/src/server.js
@@ -4,6 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var app_1 = __importDefault(require("./app"));
-app_1.default.listen(3333, function () {
-    console.log('🚀 Server started on port 3333!');
+var port = process.env.PORT || 3333;
+app_1.default.listen(port, function () {
+    console.log("\uD83D\uDE80 Server started on port " + port + "!");
 });
