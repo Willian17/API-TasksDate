2f274b54fab45aa5c3f15f279b87f2223e9758ec
Willian17 <willianfernandes113@gmail.com>
2021-05-28 11:24:31 -0400

    removendo cors

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ import AppError from './errors/AppError';
 import './database';
 
 const app = express();
-app.use(cors({origin: process.env.FRONTEND_URL}))
+app.use(cors())
 
 app.use(express.json());
 app.use(routes);
